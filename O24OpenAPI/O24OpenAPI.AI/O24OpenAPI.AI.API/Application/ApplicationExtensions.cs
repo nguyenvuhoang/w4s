c23b7cf3a23ba187aac1db9fdb015fa8edb7333d
anhben <nguyenvuhoangz@gmail.com>
2025-12-14 20:34:57 +0700

    no message

--- O24OpenAPI/O24OpenAPI.AI/O24OpenAPI.AI.API/Application/ApplicationExtensions.cs
+++ O24OpenAPI/O24OpenAPI.AI/O24OpenAPI.AI.API/Application/ApplicationExtensions.cs
@@ -1,10 +1,13 @@
-﻿namespace O24OpenAPI.AI.API.Application;
+﻿using O24OpenAPI.Web.Framework.Abstractions;
+
+namespace O24OpenAPI.AI.API.Application;
 
 internal static class ApplicationExtensions
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddLinKitCqrs();
+        services.AddSingleton<IWorkflowStepInvoker, Workflow.Generated.WorkflowStepInvoker>();
         services.AddKeyedScoped<LinKit.Core.Cqrs.IMediator, LinKit.Generated.Cqrs.Mediator>("AIService");
         return services;
     }
