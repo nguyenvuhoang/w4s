2fb50235c30c4cee0448d476f32366d122c2bceb
anhben <nguyenvuhoangz@gmail.com>
2025-12-14 11:17:28 +0700

    no message

---
+++ O24OpenAPI/O24OpenAPI.AI/O24OpenAPI.AI.Domain/AggregatesModel/QdrantAggreate/QdrantUpsertRequest.cs
@@ -0,0 +1,27 @@
+﻿namespace O24OpenAPI.AI.Domain.AggregatesModel.QdrantAggreate
+{
+    public record QdrantUpsertRequest(
+      string TenantId,
+      string DocType,
+      string Language,
+      string DocId,
+      string Title,
+      int ChunkIndex,
+      string Content,
+      float[] Embedding,
+      string SourceUri = null,
+      string Version = null,
+      string[] Tags = null,
+      string[] AclRoles = null
+);
+
+    public record QdrantSearchRequest(
+      string TenantId,
+      float[] QueryEmbedding,
+      int TopK = 5,
+      string DocType = null,
+      string Language = null,
+      string[] RequireAnyRole = null
+    );
+
+}
---
+++ O24OpenAPI/O24OpenAPI.AI/O24OpenAPI.AI.Infrastructure/Configurations/QdrantSettingConfig.cs
@@ -0,0 +1,11 @@
+﻿using O24OpenAPI.Core.Configuration;
+
+namespace O24OpenAPI.O24AI.Configuration
+{
+    public class QdrantSettingConfig : IConfig
+    {
+        public string ClientName { get; set; }
+        public string Endpoints { get; set; }
+        public int TimeoutInSeconds { get; set; }
+    }
+}
