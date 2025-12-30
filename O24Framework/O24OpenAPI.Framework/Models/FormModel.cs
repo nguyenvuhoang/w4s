5bb0dd38492abfc61d72985a626fd94b93b3aa78
anhben <nguyenvuhoangz@gmail.com>
2025-12-30 20:03:43 +0700

    Commit for CMS Load Form Controller

--- O24Framework/O24OpenAPI.Framework/Models/FormModel.cs
+++ O24Framework/O24OpenAPI.Framework/Models/FormModel.cs
@@ -1,6 +1,6 @@
-using System.Text.Json;
 using Newtonsoft.Json;
 using O24OpenAPI.Client.Events.EventData;
+using System.Text.Json;
 
 namespace O24OpenAPI.Framework.Models;
 
---
+++ O24OpenAPI/O24OpenAPI.CMS/O24OpenAPI.CMS.API/Controllers/FormController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using O24OpenAPI.CMS.API.Application.Services.Interfaces;
+using O24OpenAPI.Framework.Controllers;
+
+namespace O24OpenAPI.CMS.API.Controllers;
+
+public class FormController(ILoadFormService formService) : BaseController
+{
+    private readonly ILoadFormService _formService = formService;
+    [HttpPost]
+    public virtual async Task<IActionResult> Load([FromBody] FormModelRequest request)
+    {
+        await _formService.LoadFormAndRoleTask(request);
+        return Ok(request);
+    }
+}
