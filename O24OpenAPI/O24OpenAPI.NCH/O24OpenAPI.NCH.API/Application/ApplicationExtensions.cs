5f002e1af8a3bc2e479b36fd23d94230508f367e
anhben <nguyenvuhoangz@gmail.com>
2026-01-02 10:08:06 +0700

    inject nch

---
+++ O24OpenAPI/O24OpenAPI.NCH/O24OpenAPI.NCH.API/Application/ApplicationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using O24OpenAPI.Framework.Abstractions;
+
+namespace O24OpenAPI.NCH.API.Application;
+
+internal static class ApplicationExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddLinKitCqrs("nch");
+        services.AddKeyedSingleton<IWorkflowStepInvoker, Workflow.Generated.WorkflowStepInvoker>(serviceKey: "nch");
+        return services;
+    }
+}
