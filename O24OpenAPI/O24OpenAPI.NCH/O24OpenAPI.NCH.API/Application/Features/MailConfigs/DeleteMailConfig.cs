a4be03dc06c4e2ea9476538e44ffc97da2df6f3c
quoc_linh <quoclinhnq@gmail.com>
2026-01-02 20:44:54 +0700

    feat(DeleteMailConfig): implement command and handler for deleting mail configurations

---
+++ O24OpenAPI/O24OpenAPI.NCH/O24OpenAPI.NCH.API/Application/Features/MailConfigs/DeleteMailConfig.cs
@@ -0,0 +1,28 @@
+﻿using LinKit.Core.Cqrs;
+using O24OpenAPI.APIContracts.Constants;
+using O24OpenAPI.Framework.Attributes;
+using O24OpenAPI.Framework.Models;
+using O24OpenAPI.NCH.Domain.AggregatesModel.MailAggregate;
+
+namespace O24OpenAPI.NCH.API.Application.Features.MailConfigs;
+
+public class DeleteMailConfigCommand : BaseTransactionModel, ICommand<bool>
+{
+    public int Id { get; set; }
+}
+
+[CqrsHandler]
+public class DeleteMailConfigHandler(IMailConfigRepository mailConfigRepository)
+    : ICommandHandler<DeleteMailConfigCommand, bool>
+{
+    [WorkflowStep(WorkflowStep.NCH.WF_STEP_NCH_DELETE_MAIL_CONFIG)]
+    public async Task<bool> HandleAsync(
+        DeleteMailConfigCommand request,
+        CancellationToken cancellationToken = default
+    )
+    {
+        MailConfig mailConfig = await mailConfigRepository.GetById(request.Id);
+        await mailConfigRepository.Delete(mailConfig);
+        return true;
+    }
+}
