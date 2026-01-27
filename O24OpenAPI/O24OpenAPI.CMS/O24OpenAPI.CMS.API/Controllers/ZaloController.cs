3b0f1cf94fa3cf5e4621ecfec295742bcefd69b8
anhben <nguyenvuhoangz@gmail.com>
2026-01-27 22:29:48 +0700

    Thêm zalocontroler

--- O24OpenAPI/O24OpenAPI.CMS/O24OpenAPI.CMS.API/Controllers/ZaloController.cs
+++ O24OpenAPI/O24OpenAPI.CMS/O24OpenAPI.CMS.API/Controllers/ZaloController.cs
@@ -1,33 +1,35 @@
-//using LinKit.Core.Cqrs;
-//using Microsoft.AspNetCore.Mvc;
-//using O24OpenAPI.APIContracts.Constants;
-//using O24OpenAPI.CMS.API.Models.Zalo;
-//using O24OpenAPI.Framework.Controllers;
-//namespace O24OpenAPI.CMS.API.Controllers;
+using LinKit.Core.Cqrs;
+using Microsoft.AspNetCore.Mvc;
+using O24OpenAPI.APIContracts.Constants;
+using O24OpenAPI.CMS.API.Application.Features.Zalo;
+using O24OpenAPI.CMS.API.Models.Zalo;
+using O24OpenAPI.Framework.Controllers;
 
-//public class ZaloController([FromKeyedServices(MediatorKey.CMS)] IMediator mediator) : BaseController
-//{
+namespace O24OpenAPI.CMS.API.Controllers;
 
-//    [HttpPost("/api/zalo/create")]
-//    public virtual async Task<IActionResult> Create(
-//        [FromBody] ZaloProcessModel request,
-//        CancellationToken cancellationToken
-//    )
-//    {
-//        APIContracts.Models.PMT.ZaloTokenResponseModel result = await mediator.SendAsync(request.ToZaloExchangeTokenCommand(), cancellationToken);
-//        return Ok(result);
-//    }
+public class ZaloController([FromKeyedServices(MediatorKey.CMS)] IMediator mediator) : BaseController
+{
 
-//    [HttpGet("/api/zalo/return")]
-//    public virtual async Task<IActionResult> Return(
-//        [FromQuery] ZaloProcessModel request,
-//        CancellationToken cancellationToken
-//    )
-//    {
-//        string oaid = request.OaId ?? string.Empty;
-//        string code = request.Code ?? string.Empty;
+    [HttpPost]
+    public virtual async Task<IActionResult> Create(
+        [FromBody] GenerateAccessTokenCommand request,
+        CancellationToken cancellationToken
+    )
+    {
+        var result = await mediator.SendAsync(request, cancellationToken);
+        return Ok(result);
+    }
 
-//        return Ok();
-//    }
+    [HttpGet]
+    public virtual async Task<IActionResult> Return(
+        [FromQuery] ZaloProcessModel request,
+        CancellationToken cancellationToken
+    )
+    {
+        string oaid = request.OaId ?? string.Empty;
+        string code = request.Code ?? string.Empty;
 
-//}
+        return Ok();
+    }
+
+}
